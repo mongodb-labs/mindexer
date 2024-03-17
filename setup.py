b4cc747368f741603ef686553fa41bedd897a09b
Thomas Rueckstiess <thomas@mongodb.com>
2024-03-18 09:31:22 +1100

    bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="mindexer",
-    version="0.3.1",
+    version="0.3.2",
     description="Experimental Index Recommendation Tool for MongoDB",
     long_description=readme(),
     long_description_content_type="text/markdown",
